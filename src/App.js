2cf09290e3774e423da2ce50b557317c2c8bf334
isaac-levine <80065258+isaac-levine@users.noreply.github.com>
2022-09-20 23:43:59 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,17 +5,8 @@ import './App.css';
 
 function App() {
 
-  var shortStick = false
-
-  const firstQuestion = {
-    questionText: "What type of stick do you use?",
-    answerOptions: [
-      "Short stick", "Long stick"
-    ]
-  }
-
-  // Series of 6 questions that we ask once we know user is looking for a SHORT stick
-  const shortStickQuestions = [
+  // Series of 6 questions that we ask
+  const questions = [
     {
       questionText: "What's your style of play?",
       answerOptions: ["Feeder", "Dodger", "Finisher"]
@@ -30,7 +21,7 @@ function App() {
     }, 
     {
       questionText: "What is your dodging style?",
-      answerOptions: ["Power dodge", "Speed dodge"]
+      answerOptions: ["Power dodge", "Skill dodge", "Speed dodge"]
     }, 
     {
       questionText: "Where do you like to attack from?",
@@ -42,64 +33,65 @@ function App() {
     }
   ]
 
-  // Series of 5 questions that we ask once we know user is looking for a LONG stick
-  const longStickQuestions = [
-    {
-      questionText: "What's your style of play?",
-      answerOptions: ["Aggressive - Strip the ball", "Fundamental - Body position", "Outlet passes", "Creating transition"]
-    }, 
-    {
-      questionText: "What's on your stat sheet?",
-      answerOptions: ["Caused turnovers", "Ground balls", "Goals and assists", "Penality minutes"]
-    },
-    {
-      questionText: "Where do you defend from?",
-      answerOptions: ["Above GLE", "Below GLE"]
-    }, 
-    {
-      questionText: "What's your favorite check to throw?",
-      answerOptions: ["Wrap check", "V hold", "Body check", "Poke check"]
-    }, 
-    {
-      questionText: "What defensive position do you play?",
-      answerOptions: ["LSM", "Close defense"]
-    }, 
-  ]
 
   // state object to track which question we are on 
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  const [showScore, setShowScore] = useState(false)
+  const [showResult, setShowResult] = useState(false)
+
+  const [kinetikVote, setKinetikVote] = useState(0);
+  const [kinetikAnswers, setKinetikAnswers] = useState(["Finisher", "Goals", "Outside shots", "Power dodge", "Attack", "Crease"]);
+
+  const [hyperVote, setHyperVote] = useState(0);
+  const [hyperAnswers, setHyperAnswers] = useState(["Feeder", "Assists", "Inside moves", "Speed dodge", "X", "Attack"]);
+
+  const [evoVote, setEvoVote] = useState(0);
+  const [evoAnswers, setEvoAnswers] = useState(["Dodger", "Even", "Feeds", "Skill dodge", "Both attack and midfield"]);
+
+  const [winner, setWinner] = useState("None");
 
   // change to the next question
-  const handleAnswerButtonClick = () => {
+  const handleAnswerButtonClick = (answerOption) => {
     const nextQuestion = currentQuestion + 1
-    if (nextQuestion < shortStickQuestions.length){
+
+    if (kinetikAnswers.includes(answerOption)) {
+      setKinetikVote(kinetikVote+1);
+    } else if (hyperAnswers.includes(answerOption)) {
+      setHyperVote(hyperVote+1);
+    } else if (evoAnswers.includes(answerOption)) {
+      setEvoVote(evoAnswers+1);
+    }
+
+    if (kinetikVote >= hyperVote >= evoVote) {
+      setWinner("Maverick Kinetick 2.0");
+    } else if (hyperVote >= kinetikVote >= evoVote) {
+      setWinner("STX Hyper Power");
+    } else setWinner("Warrior Evo QX-O");
+
+
+    if (nextQuestion < questions.length){
       setCurrentQuestion(nextQuestion);
     } else {
-      setShowScore(true)
+      setShowResult(true)
     }
+
   }
-
-
 
   return (
 		<div className='app'>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
-			{showScore ? (
-				<div className='score-section'>Based on your responses, the stick recommend is... </div>
+			{showResult ? (
+				<div className='result-section'>Based on your responses, the stick recommend is: {winner}</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
 							<span>Question {currentQuestion+1}</span>/6
 						</div>
-						<div>{shortStickQuestions[currentQuestion].questionText}</div>
+						<div>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
-						{shortStickQuestions[currentQuestion].answerOptions.map((answerOptions) => 
-            <button onClick={handleAnswerButtonClick}>{answerOptions}</button>)}
+						{questions[currentQuestion].answerOptions.map((answerOption) => 
+            <button onClick={() => handleAnswerButtonClick(answerOption)}>{answerOption}</button>)}
 					</div>
 				</>
 			)}
